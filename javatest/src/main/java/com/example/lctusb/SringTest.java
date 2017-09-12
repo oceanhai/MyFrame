b0e66aae1786c08ca8f92d7e2d8239531ab2b617
wuhai <wuh@lecuntao.com>
2017-09-12 13:36:21 +0800

    提交

---
+++ javatest/src/main/java/com/example/lctusb/SringTest.java
@@ -0,0 +1,17 @@
+package com.example.lctusb;
+
+/**
+ * Created by wuhai on 2017/09/08 10:54.
+ * 描述：
+ */
+
+public class SringTest {
+    
+    public static void main(String[] args){
+        String str = "abc";
+        method01(str);
+    }
+
+    private static void method01(String str) {
+    }
+}
