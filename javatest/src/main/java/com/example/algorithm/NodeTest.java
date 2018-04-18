fb44144aa993ec2e5e03da98d889d67e4b5eaae5
wuhai <wuh@lecuntao.com>
2018-04-18 18:05:43 +0800

    布局：权重 和 多叉树算法

--- javatest/src/main/java/com/example/algorithm/NodeTest.java
+++ javatest/src/main/java/com/example/algorithm/NodeTest.java
@@ -10,11 +10,17 @@ import java.util.Queue;
  */
 public class NodeTest {
     public static void main(String[] args){
+        Node node10 = new Node(10, null);
+        Node node11 = new Node(11, null);
         Node node8 = new Node(8, null);
-        Node node9 = new Node(9, null);
         Node node6 = new Node(6, null);
         Node node7 = new Node(7, null);
         Node node4 = new Node(4, null);
+
+        List<Node> list9 = new ArrayList<>();
+        list9.add(node10);
+        list9.add(node11);
+        Node node9 = new Node(9, list9);
 
         List<Node> list5 = new ArrayList<>();
         list5.add(node8);
@@ -36,13 +42,17 @@ public class NodeTest {
         list1.add(node4);
         Node node1 = new Node(1,list1);
 
-        search1(node1);
-//        search2(node1);
+//        search1(node1);
+        search2(node1);
     }
 
     /**
      * 打印 节点信息num
      * 深度优先
+     * 深度优先遍历图的方法是，从图中某顶点v出发：
+     *（1）访问顶点v；
+     *（2）依次从v的未被访问的邻接点出发，对图进行深度优先遍历；直至图中和v有路径相通的顶点都被访问；
+     *（3）若此时图中尚有顶点未被访问，则从一个未被访问的顶点出发，重新进行深度优先遍历，直到图中所有顶点均被访问过为止。
      * @param node
      */
     public static void search1(Node node){
