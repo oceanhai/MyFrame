8eabfc09386e8848c30b24c9f56834e386c9bebe
oceanhai <wuhaimengxiang@163.com>
2018-07-09 11:58:20 +0800

    分辨率 适配的一个app  验证

--- app/src/main/java/com/myframe/www/MainActivity.java
+++ app/src/main/java/com/myframe/www/MainActivity.java
@@ -3,6 +3,8 @@ package com.myframe.www;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -230,7 +232,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         pareIntent();
         initListener();
         goToActivity();
+        getDensity();
+    }
 
+
+    private void getDensity() {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        Log.d("pingmu_xy","Density is "+displayMetrics.density+" densityDpi is "+displayMetrics.densityDpi+" height: "+displayMetrics.heightPixels+
+                " width: "+displayMetrics.widthPixels);
     }
 
     private void initListener() {
